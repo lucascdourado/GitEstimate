b88032f4d59a2477ffff24c9bddc54a7f9ddb3f0
DOURADO, Lucas <lucascdourado@gmail.com>
2024-01-09 14:32:12 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export const siteConfig = {
   name: "GitEstimate",
   description: "GitEstimate - Github Estimate worth genereator ",
   ogImage: "https://mdTaquiImam.vercel.app/og-image.png",
-  url: "https://gitestimate.vercel.app",
+  url: "git-estimate-dourado.vercel.app",
 }
 
 
@@ -17,7 +17,7 @@ export default function Home() {
   
 <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#7ba0f2_1px,transparent_1px)] [background-size:16px_16px]"></div>
    <InputForm/>
-   <Link href="https://github.com/taqui-786/GitEstimate" target="_blank" className=" w-28 z-50 bg-primary absolute -top-[11px] -right-[38px] flex items-center justify-center p-5 rotate-45"><Github className='h-5 w-5 text-white' /></Link>
+   <Link href="https://github.com/lucascdourado/GitEstimate" target="_blank" className=" w-28 z-50 bg-primary absolute -top-[11px] -right-[38px] flex items-center justify-center p-5 rotate-45"><Github className='h-5 w-5 text-white' /></Link>
    </main>
   )
 }
